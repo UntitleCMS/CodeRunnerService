5fc68be1d390ba63c490b038b4d8b54654efa260
VillSource <anirut1800@gmail.com>
2024-01-07 02:04:50 +0700

    fillter only supported lang

--- src/interceptors/clear-process/clear-process.interceptor.ts
+++ src/interceptors/clear-process/clear-process.interceptor.ts
@@ -1,18 +1,36 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { Language, SourceCodeModel } from '@app/core';
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, of } from 'rxjs';
 import { Socket } from 'socket.io';
 import { SocketData } from 'src/models/socket-data';
+
+const languages = Object.getOwnPropertyNames(Language)
+  .filter((n) => n != 'name' && n != 'length' && n != 'prototype')
+  .map((n) => Language[n]);
 
 @Injectable()
 export class ClearProcessInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    console.log(">> Clear Process Interceptor");
-    const socketData:SocketData = context.switchToWs().getClient().data;
-    if (socketData.process){
+    console.log('>> Clear Process Interceptor');
+    const socketData: SocketData = context.switchToWs().getClient().data;
+    if (socketData.process) {
       socketData.process.kill();
       socketData.process.complete();
       socketData.process = null;
     }
-    return next.handle();
+
+    const data: SourceCodeModel = context.switchToWs().getData();
+    if(languages.includes(data.language))
+      return next.handle();
+    
+    
+    const socket: Socket = context.switchToWs().getClient();
+    socket.emit("error", `Language ${data.language} is not supported.`);
+    return of();
   }
 }
