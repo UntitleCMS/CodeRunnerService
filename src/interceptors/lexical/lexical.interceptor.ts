41c36c75ba5f577d5b1dc6c04ab37d287c8bce2f
VillSource <anirut1800@gmail.com>
2024-01-07 01:19:40 +0700

    implement lexical fillter

--- src/interceptors/lexical/lexical.interceptor.ts
+++ src/interceptors/lexical/lexical.interceptor.ts
@@ -1,6 +1,7 @@
 import { SourceCodeModel } from '@app/core';
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { Socket } from 'socket.io';
 import { LexicalFactory } from 'src/factories/lexical.factory';
 
 @Injectable()
@@ -8,14 +9,18 @@ export class LexicalInterceptor implements NestInterceptor {
   constructor(private readonly lexicalFactory: LexicalFactory){}
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    console.log(">> Lexical intercept");
     const data = context.switchToWs().getData() as SourceCodeModel;
-
+    
     const v = this.lexicalFactory.create(data.language);
     const isValid = v.verify(data);
 
-    console.log("Is valid: " + isValid);
+    console.log(">> Lexical intercept :", isValid);
     
-    return next.handle();
+    if (isValid)
+      return next.handle();
+    
+    const socket:Socket = context.switchToWs().getClient();
+    socket.emit("error", "Syntax error");
+    return of();
   }
 }
