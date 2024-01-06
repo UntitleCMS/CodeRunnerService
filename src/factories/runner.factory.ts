9262d0b3b0f384b01bd01ef47983f5d59f7fec6f
VillSource <anirut1800@gmail.com>
2024-01-07 00:57:59 +0700

    add env

--- src/factories/runner.factory.ts
+++ src/factories/runner.factory.ts
@@ -1,15 +1,20 @@
 import { Language, SshConnectionConfig } from '@app/core';
 import { PythonRunner } from '@app/python';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class RunnerFactory {
+
+  constructor(private readonly config: ConfigService){}
+
   create(language: string) {
+    const config = new SshConnectionConfig();
+    config.key = this.config.get('PYTHON_SANDBOX_KEY');
+    config.port = this.config.get('PYTHON_SANDBOX_PORT');
+    config.username = this.config.get('PYTHON_SANDBOX_USER');
+
     if (language == Language.Python3){
-      const config = new SshConnectionConfig();
-      config.key = 'C:\\Users\\Anirut\\Desktop\\MyFnPj\\code-runner\\CompilerContainer\\Keys\\id_rsa';
-      config.port = 2222;
-      config.username = 'runner';
       return new PythonRunner(config);
     }
     else throw new Error(`Language '${language}' is not supported`);
