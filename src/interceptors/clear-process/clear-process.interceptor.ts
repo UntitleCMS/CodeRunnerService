fd578ec8bcdc619fc1e1d9684c1e5a5ef90d1b75
VillSource <anirut1800@gmail.com>
2024-01-07 01:06:40 +0700

    add logic

--- src/interceptors/clear-process/clear-process.interceptor.ts
+++ src/interceptors/clear-process/clear-process.interceptor.ts
@@ -1,11 +1,18 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { Socket } from 'socket.io';
+import { SocketData } from 'src/models/socket-data';
 
 @Injectable()
 export class ClearProcessInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     console.log(">> Clear Process Interceptor");
-    
+    const socketData:SocketData = context.switchToWs().getClient().data;
+    if (socketData.process){
+      socketData.process.kill();
+      socketData.process.complete();
+      socketData.process = null;
+    }
     return next.handle();
   }
 }
