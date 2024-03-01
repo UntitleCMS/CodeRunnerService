51900cbdf78c8048a918b8ac8dde3ca2a0a7195b
VillSource <anirut1800@gmail.com>
2024-03-01 23:33:29 +0700

    fix : slow down for output

--- src/event/processIO.listenner.ts
+++ src/event/processIO.listenner.ts
@@ -1,14 +1,45 @@
 import { IOType, ProcessObservable } from '@app/core';
+import { buffer, bufferTime, from, map, mergeMap, of, tap } from 'rxjs';
 import { Socket } from 'socket.io';
 
-export function redirectOputputTo(socket: Socket, processIO: ProcessObservable) {
+export function redirectOputputTo(
+  socket: Socket,
+  processIO: ProcessObservable,
+) {
   console.info('Redirecting output to client : ', socket.id);
-  processIO.subscribe((io: IOType) => {
-    if (io.type == 'stderr' || io.type == 'stdout' || io.type == 'exit') {
-      socket.emit(io.type, io.data);
-    }
-    if (io.type == 'exit' && io.data == 124){
-      socket.emit('error', "Timeout");
-    }
-  });
+  processIO
+    .pipe(
+      bufferTime(100),
+      mergeMap((buff) => {
+        {
+          let x: IOType[] = [];
+
+          for (let i = 0; i < buff.length; i++) {
+            const item = buff[i];
+            if (item.type != 'stdout' || x.length == 0) {
+              x.push(item);
+            } else {
+              x[x.length - 1].data += item.data;
+            }
+          }
+
+          let io = from(x);
+          return io;
+        }
+      }),
+      map(i=>{
+        if(i.type!='stdout' || i.data.length <= 1000) return i;
+        else {
+          return {...i, data: i.data.slice(0,1000)+'[buffer...]'}
+        }
+      })
+    )
+    .subscribe((io: IOType) => {
+      if (io.type == 'stderr' || io.type == 'stdout' || io.type == 'exit') {
+        socket.emit(io.type, io.data);
+      }
+      if (io.type == 'exit' && io.data == 124) {
+        socket.emit('error', 'Timeout');
+      }
+    });
 }
