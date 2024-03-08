4b245ea290c4b145dd2cdd8b09d520ea26f8bd77
VillSource <anirut1800@gmail.com>
2024-03-08 13:32:27 +0700

    mock code scaner factory

--- src/factories/code-scaner.factory.ts
+++ src/factories/code-scaner.factory.ts
@@ -7,10 +7,17 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class CodeScanerFactory {
   create(language: string): SourceCodeFeaturBlockingStrategy {
-    // if(language == Language.Python3)
-    //   return { scan(code) { return true; }, };
-    // if (language == Language.Java17)
-    //   return { scan(code) { return true; }, };
+    const mock = {
+      scan() {
+        return { isSucured: true, msg: 'mock' };
+      },
+      isSecure() {
+        return true;
+      },
+    };
+    
+    if (language == Language.Python3) return mock;
+    if (language == Language.Java17) return mock;
     if (language == Language.C12) return new C12Scaner();
     else throw new Error(`Language '${language}' is not supported.`);
   }
